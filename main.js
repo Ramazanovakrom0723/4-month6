833e640f33c41ed832cf39272d4a05afd387ba84
Ramazanovakrom0723 <akromramazanov77@gmail.com>
2024-03-28 00:53:59 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+// 1-masala
+
+// let obj = {namae: "Chevrolet Nexia", year: 2014, cost:"5500$", condition:"Ideal"}
+// let obj_keys = Object.keys(obj)
+// let obj_values = Object.values(obj)
+// console.log(obj_keys)
+// console.log(obj_values)
+
+
+
+
+
+// 2-masala
+
+// const number = (number) => {
+//     let arr = [1,2,3,5,6,7,8,46,34]
+//     if(arr.includes(number)) {
+//         console.log(`berilgan sonlar ichida ${number} bor`)
+//     }else{
+//         console.log(`berilgan sonlar ichida ${number} yoq`)
+//     }
+// }
+// number (8)
+
+
+
+
+
+// 3-masala
+
+// const string = (name) => {
+//     let arr = ["akrom"; "sunnat"; "nodir"; "shagzod"; "dovlat"]
+//     if(arr.includes(name)) {
+//         console.log(`berilgan ismlar ichida ${name} bor`)
+//     }else{
+//         console.log(`berilgan ismlar ichida ${name} yoq`)
+//     }
+// }
+// string ("akrom")
+
+
+
+
+
+// 5-masala
+
+// let user = {name: "akrom", Locatio: "toshkent", age: 19}
+// let student = {is_worker: false, is_student: true}
+// let sex = {is_male: true}
+// let returned_Object = Object.assign(user, student, sex)
+// console.log(returned_Object);
